b31ec211db5c657a689f44da89fa0633cbd5323e
Carlos Barata <cbarata73@gmail.com>
2025-01-21 12:48:31 +0000

    Apelido
    Coloquei o apelido no código.

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ print("Ola " + nome + "!")
 
 idade = input("Qual a tua idade: ")
 print("Tu tens " + idade + " anos de idade.")
+
+apelido = input("Qual o teu apelido: ")
+print("O teu nome completo e " + nome + " " + apelido + ".")
